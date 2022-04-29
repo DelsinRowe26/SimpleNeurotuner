c58588787ae073a201add900e3b7666ed681a11e
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-04-29 22:27:18 +0600

    Добавьте файлы проекта.

---
+++ SimpleNeurotuner/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CSCore;
+using CSCore.SoundIn;//Вход звука
+using CSCore.SoundOut;//Выход звука
+using CSCore.CoreAudioAPI;
+using CSCore.Streams;
+using CSCore.Codecs;
+using CSCore.Streams.Effects;
+using CSCore.DSP;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace SimpleNeurotuner
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SimpleMixer mMixer;
+        private int SampleRate = 44100;
+        private ISoundOut _soundOut;
+        private WasapiCapture _soundIn;
+        private IWaveSource _source;
+        private MMDeviceCollection mOutputDevices;
+        private MMDeviceCollection mInputDevices;
+        private PitchShifter _pitchShifter;
+        private ISampleSource mMp3;
+        private OpenFileDialog openFileDialog;
+        private string file;
+        private int click;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Mixer()
+        {
+            mMixer = new SimpleMixer(2, SampleRate) //стерео, 44,1 КГц
+            {
+                FillWithZeros = true,
+                DivideResult = true, //Для этого установлено значение true, чтобы избежать звуков тиков из-за превышения -1 и 1.
+            };
+        }
+
+        private void SimpleNeurotuner_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Находит устройства для захвата звука и заполнияет комбобокс
+            MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+            mInputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Capture, DeviceState.Active);
+            MMDevice activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+            foreach (MMDevice device in mInputDevices)
+            {
+                cmbInput.Items.Add(device.FriendlyName);
+                if (device.DeviceID == activeDevice.DeviceID) cmbInput.SelectedIndex = cmbInput.Items.Count - 1;
+            }
+
+            //Находит устройства для вывода звука и заполняет комбобокс
+            activeDevice = deviceEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            mOutputDevices = deviceEnum.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active);
+            foreach (MMDevice device in mOutputDevices)
+            {
+                cmbOutput.Items.Add(device.FriendlyName);
+                if (device.DeviceID == activeDevice.DeviceID) cmbOutput.SelectedIndex = cmbOutput.Items.Count - 1;
+            }
+        }
+
+        private async void btnStart_Open_Click(object sender, RoutedEventArgs e)
+        {
+            click = 1;
+            openFileDialog = new OpenFileDialog()
+            {
+                Filter = CodecFactory.SupportedFilesFilterEn,
+                Title = "Select a file..."
+            };
+            file = openFileDialog.FileName;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                await Task.Run(() => Sound(file));
+            }
+        }
+
+        private void Stop()
+        {
+
+            if (_soundOut != null)
+            {
+                _soundOut.Stop();
+                _soundOut.Dispose();
+                _soundOut = null;
+            }
+            if (_soundIn != null)
+            {
+                _soundIn.Stop();
+                _soundIn.Dispose();
+                _soundIn = null;
+            }
+            if (_source != null)
+            {
+                _source.Dispose();
+                _source = null;
+            }
+        }
+
+        private void Sound(string file)
+        {
+            Stop();
+                do
+                {
+                    /*Mixer();
+                    mMp3 = CodecFactory.Instance.GetCodec(openFileDialog.FileName).ToStereo().ToSampleSource();
+                    mMixer.AddSource(mMp3.ChangeSampleRate(mMixer.WaveFormat.SampleRate));*/
+                    //open the selected file
+                    ISampleSource source = CodecFactory.Instance.GetCodec(openFileDialog.FileName)
+                        .ToSampleSource()
+                        .AppendSource(x => new PitchShifter(x), out _pitchShifter);
+
+                    //play the audio
+
+                    _soundOut = new WasapiOut();
+                    _soundOut.Initialize(source.ToWaveSource(16));
+                    _soundOut.Play();
+                    //InitializeComponent();
+                    //Thread.Sleep(200);*/
+                } while (click != 0);
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            Stop();
+            click = 0;
+        }
+
+        private void SimpleNeurotuner_Closing(object sender, CancelEventArgs e)
+        {
+            Stop();
+        }
+    }
+}
