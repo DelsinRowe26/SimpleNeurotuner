d9e41e70e6fcaab8942c2264242ce01d283fe398
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-05-18 21:02:58 +0600

    Add

---
+++ SimpleNeurotuner/WavFileUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CSCore;
+using CSCore.Codecs.WAV;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNeurotuner
+{
+    public static class WavFileUtils
+    {
+        public static void TrimWavFile(string inPath, string outPath, TimeSpan cutFromStar, TimeSpan cutFromEnd)
+        {
+            using(WaveFileReader reader = new WaveFileReader(inPath))
+            {
+                using(WaveWriter writer = new WaveWriter(outPath, reader.WaveFormat))
+                {
+                    int bytesPerMillisecond = reader.WaveFormat.BytesPerSecond / 1000;
+
+                    int startPos = (int)cutFromStar.TotalMilliseconds * bytesPerMillisecond;
+                    startPos = startPos - startPos % reader.WaveFormat.BlockAlign;
+                }
+            }
+        }
+    }
+}
