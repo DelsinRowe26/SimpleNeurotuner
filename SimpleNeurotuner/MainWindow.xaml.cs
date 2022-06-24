be6f4575daeb9284ff755c7a81034b05f9b377b9
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2022-06-24 12:44:04 +0600

    Add

--- SimpleNeurotuner/MainWindow.xaml.cs
+++ SimpleNeurotuner/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ using System.IO;
 using Microsoft.DirectX.DirectSound;
 using Buffer = Microsoft.DirectX.DirectSound.Buffer;
 using System.Runtime.InteropServices;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace SimpleNeurotuner
 {
@@ -28,6 +30,12 @@ namespace SimpleNeurotuner
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+            
+        [DllImport("BiblZvuk.dll", CallingConvention = CallingConvention.Cdecl)]
+        //unsafe
+          static extern int vizualzvuk(string filename,  int[] Rdat);
+        
         private FileInfo fileInfo = new FileInfo("window.tmp");
         private FileInfo fileInfo1 = new FileInfo("Data_Load.dat");
         private FileInfo FileLanguage = new FileInfo("Data_Language.dat");
@@ -43,8 +51,8 @@ namespace SimpleNeurotuner
         private IWaveSource _source;
         private MMDeviceCollection mOutputDevices;
         private MMDeviceCollection mInputDevices;
-        string start = "00:00:03,5";
-        string end = "00:00:04,5";
+        string start = "00:00:03,25";
+        string end = "00:00:04,25";
         string myfile;
         string cutmyfile;
         public int index = 1;
@@ -58,13 +66,37 @@ namespace SimpleNeurotuner
         private string[] allfile;
         private int click, audioclick = 0;
 
-        
+        BackgroundWorker worker;
 
         public MainWindow()
         {
             InitializeComponent();
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+        }
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    //e.Cancel = true;
+                    (sender as BackgroundWorker).ReportProgress(100);
+                    break;
+                    //return;
+                }
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(100);
+            }
         }
 
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            PBNFT.Value = e.ProgressPercentage;
+        }
         private void Mixer()
         {
             mMixer = new SimpleMixer(2, SampleRate) //стерео, 44,1 КГц
@@ -127,8 +159,8 @@ namespace SimpleNeurotuner
         {
             if (cmbModes.SelectedIndex == 0)
             {
-                Recording();
-                //Recordind2();
+                //Recording();
+                Recordind2();
             }
             else
             {
@@ -207,7 +239,7 @@ namespace SimpleNeurotuner
             catch (Exception ex)
             {
                 string msg = "Error in StartFullDuplex: \r\n" + ex.Message;
-                MessageBox.Show(msg);
+                System.Windows.MessageBox.Show(msg);
                 Debug.WriteLine(msg);
             }
             //return false;
@@ -229,7 +261,7 @@ namespace SimpleNeurotuner
                 Mixer();
                 mMp3 = CodecFactory.Instance.GetCodec(filename).ToStereo().ToSampleSource();
                 mDsp1 = new SampleDSP(mMp3.ToWaveSource(16).ToSampleSource());
-                //mDsp1.GainDB = (float)slVolumeRecord.Value;
+                //mDsp1.GainDB = (float)slVolume.Value;
                 mMixer.AddSource(mDsp1.ChangeSampleRate(mMixer.WaveFormat.SampleRate).ToWaveSource(16).Loop().ToSampleSource());
 
                 await Task.Run(() => SoundOut());
@@ -261,7 +293,7 @@ namespace SimpleNeurotuner
         private void btnStart_Open_MouseMove(object sender, MouseEventArgs e)
         {
             Style style = new Style();
-            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new FontFamily("Verdana") });
+            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new System.Windows.Media.FontFamily("Verdana") });
             style.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(10) });
             style.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = new SolidColorBrush(Colors.Blue) });
             style.Setters.Add(new Setter { Property = Control.ForegroundProperty, Value = new SolidColorBrush(Colors.White) });
@@ -271,7 +303,7 @@ namespace SimpleNeurotuner
         private void btnStart_Open_MouseLeave(object sender, MouseEventArgs e)
         {
             Style style = new Style();
-            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new FontFamily("Verdana") });
+            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new System.Windows.Media.FontFamily("Verdana") });
             style.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(10) });
             style.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = new SolidColorBrush(Colors.Blue) });
             style.Setters.Add(new Setter { Property = Control.ForegroundProperty, Value = new SolidColorBrush(Colors.Black) });
@@ -281,7 +313,7 @@ namespace SimpleNeurotuner
         private void btnStop_MouseMove(object sender, MouseEventArgs e)
         {
             Style style = new Style();
-            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new FontFamily("Verdana") });
+            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new System.Windows.Media.FontFamily("Verdana") });
             style.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(10) });
             style.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = new SolidColorBrush(Colors.Blue) });
             style.Setters.Add(new Setter { Property = Control.ForegroundProperty, Value = new SolidColorBrush(Colors.White) });
@@ -291,7 +323,7 @@ namespace SimpleNeurotuner
         private void btnStop_MouseLeave(object sender, MouseEventArgs e)
         {
             Style style = new Style();
-            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new FontFamily("Verdana") });
+            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new System.Windows.Media.FontFamily("Verdana") });
             style.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(10) });
             style.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = new SolidColorBrush(Colors.Blue) });
             style.Setters.Add(new Setter { Property = Control.ForegroundProperty, Value = new SolidColorBrush(Colors.Black) });
@@ -331,7 +363,7 @@ namespace SimpleNeurotuner
         private void btnRecord_MouseMove(object sender, MouseEventArgs e)
         {
             Style style = new Style();
-            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new FontFamily("Verdana") });
+            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new System.Windows.Media.FontFamily("Verdana") });
             style.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(10) });
             style.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = new SolidColorBrush(Colors.Blue) });
             style.Setters.Add(new Setter { Property = Control.ForegroundProperty, Value = new SolidColorBrush(Colors.White) });
@@ -341,7 +373,7 @@ namespace SimpleNeurotuner
         private void btnRecord_MouseLeave(object sender, MouseEventArgs e)
         {
             Style style = new Style();
-            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new FontFamily("Verdana") });
+            style.Setters.Add(new Setter { Property = Control.FontFamilyProperty, Value = new System.Windows.Media.FontFamily("Verdana") });
             style.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(10) });
             style.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = new SolidColorBrush(Colors.Blue) });
             style.Setters.Add(new Setter { Property = Control.ForegroundProperty, Value = new SolidColorBrush(Colors.Black) });
@@ -355,7 +387,7 @@ namespace SimpleNeurotuner
             Audition();
         }
 
-        private void Recording()
+        private async void Recording()
         {
             try
             {
@@ -367,15 +399,22 @@ namespace SimpleNeurotuner
                 {
                     mSoundIn.Device = mInputDevices[cmbInput.SelectedIndex];
                     mSoundIn.Initialize();
+                    lbRecordPB.Visibility = Visibility.Visible; 
                     mSoundIn.Start();
                     using (WaveWriter record = new WaveWriter(cutmyfile, mSoundIn.WaveFormat))
                     {
                         mSoundIn.DataAvailable += (s, data) => record.Write(data.Data, data.Offset, data.ByteCount);
-                        Thread.Sleep(5000);
+                        for(int i = 0; i < 100; i++)
+                        {
+                            pbRecord.Value++;
+                            await Task.Delay(50);
+                        }
+                        //Thread.Sleep(5000);
                         mSoundIn.Stop();
-                        
+                        lbRecordPB.Visibility = Visibility.Hidden;
+                        pbRecord.Value = 0;
                     }
-                    Thread.Sleep(2000);
+                    Thread.Sleep(1000);
                     CutRecord cutRecord = new CutRecord();
                     cutRecord.CutFromWave(cutmyfile, myfile, start, end);
                     File.Move(myfile, @"Record\" + myfile);
@@ -462,16 +501,20 @@ namespace SimpleNeurotuner
                 cmbModes.Items.Add("Записи");
                 cmbModes.Items.Add("Прослушивание");
                 cmbModes.SelectedIndex = cmbModes.Items.Count - 1;
-                Title = "Нейрокейс";
+                Title = "Нейротюнер NFT";
                 btnStart_Open.Content = "Старт";
                 btnStop.Content = "Стоп";
                 Help.Header = "Помощь";
-                lbVersion.Content = "Нейрокейс Версия: 1.1";
+                TabNFT.Header = "gNeuro NFT";
+                TabSettings.Header = "Настройки";
+                lbVersion.Content = "Версия: 1.1";
                 btnRecord.Content = "Прослушать";
                 cmbInput.ToolTip = "Микрофон";
                 cmbOutput.ToolTip = "Наушники";
                 cmbRecord.ToolTip = "Записи";
                 cmbModes.ToolTip = "Режимы";
+                lbPBNFT.Content = "Идёт загрузка NFT...";
+                lbRecordPB.Content = "Идёт запись...";
             }
             else
             {
@@ -484,16 +527,20 @@ namespace SimpleNeurotuner
                 cmbModes.Items.Add("Record");
                 cmbModes.Items.Add("Audition");
                 cmbModes.SelectedIndex = cmbModes.Items.Count - 1;
-                Title = "Neurokeys";
+                Title = "Neurotuner NFT";
                 btnStart_Open.Content = "Start";
                 btnStop.Content = "Stop";
                 Help.Header = "Help";
-                lbVersion.Content = "Neurokeys Version: 1.1";
+                TabNFT.Header = "gNeuro NFT";
+                TabSettings.Header = "Settings";
+                lbVersion.Content = "Version: 1.1";
                 btnRecord.Content = "Audition";
                 cmbInput.ToolTip = "Microphone";
                 cmbOutput.ToolTip = "Speaker";
                 cmbRecord.ToolTip = "Record";
                 cmbModes.ToolTip = "Modes";
+                lbPBNFT.Content = "NFT loading in progress...";
+                lbRecordPB.Content = "Recording in progress...";
             }
         }
 
@@ -529,10 +576,13 @@ namespace SimpleNeurotuner
                 CreateWindow window = new CreateWindow();
                 window.Show();
                 btnRecord.Visibility = Visibility.Visible;
+                pbRecord.Visibility = Visibility.Visible;
+                pbRecord.Value = 0;
             }
             else
             {
                 btnRecord.Visibility = Visibility.Hidden;
+                pbRecord.Visibility = Visibility.Hidden;
             }
         }
 
@@ -548,12 +598,135 @@ namespace SimpleNeurotuner
             await Task.Run(() => SoundOut());
         }
 
-        private void cmbRecord_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void cmbRecord_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (cmbRecord.SelectedItem != null)
             {
-                filename = @"Record\" + cmbRecord.SelectedItem.ToString();
+                //unsafe
+                {
+                    int[] Rdat = new int[150000];
+                    int Ndt;
+                    int Ww, Hw,k,ik,dWw,dHw;
+                    filename = @"Record\" + cmbRecord.SelectedItem.ToString();
+                    if ((filename != "Record\\Select a record") && (filename != "Record\\Выберите запись"))
+                    {
+                        worker.RunWorkerAsync();
+                        Ndt = await Task.Run(() =>
+                        {
+                            return vizualzvuk(filename, Rdat);
+                        });                        
+                        Hw = (int)Math.Sqrt(Ndt);
+                        Ww = (int)((double)(Ndt) / (double)(Hw) + 0.5);
+                        dWw = (int)((Image1.Width - (double)Ww) / 2.0)-5;
+                        if (dWw < 0)
+                            dWw = 0;
+                        dHw = (int)((Image1.Height - (double)Hw) / 2.0)-5;
+                        if (dHw < 0)
+                            dHw = 0;
+                        WriteableBitmap wb = new WriteableBitmap((int)Image1.Width, (int)Image1.Height, Ww, Hw, PixelFormats.Bgra32, null);
+
+                        // Define the update square (which is as big as the entire image).
+                        Int32Rect rect = new Int32Rect(0, 0, (int)Image1.Width, (int)Image1.Height);
+
+                        byte[] pixels = new byte[(int)Image1.Width * (int)Image1.Height * wb.Format.BitsPerPixel / 8];
+                        //Random rand = new Random();
+                        k = 0;
+                        ik = 0;
+                        int Wwt = 2, Hwt = 2, it0 = Ww / 2, jt0 = Hw / 2, it = 0, jt = 0;
+                        int R=0,G=0,B=0,A=0;
+                        int pixelOffset,poffp=0,kt=0;
+                        while (k < Ndt)
+                        {
+                            if (ik % 4 == 0)
+                            {
+                                R = Rdat[3 * k];
+                                G = Rdat[3 * k + 1];
+                                B = Rdat[3 * k + 2];
+                                A = 255;
+                                pixelOffset = (dWw + it0 + it + (dHw + jt0 + jt) * wb.PixelWidth) * wb.Format.BitsPerPixel / 8;
+                                pixels[pixelOffset] = (byte)B;
+                                pixels[pixelOffset + 1] = (byte)G;
+                                pixels[pixelOffset + 2] = (byte)R;
+                                pixels[pixelOffset + 3] = (byte)A;                               
+                                jt++;
+                                if (jt == Hwt)
+                                {
+                                    ik++;
+                                }                                
+                            }
+                            else if (ik % 4 == 1)
+                            {
+                                R = Rdat[3 * k];
+                                G = Rdat[3 * k + 1];
+                                B = Rdat[3 * k + 2];
+                                A = 255;
+                                pixelOffset = (dWw + it0 + it + (dHw + jt0 + jt) * wb.PixelWidth) * wb.Format.BitsPerPixel / 8;
+                                pixels[pixelOffset] = (byte)B;
+                                pixels[pixelOffset + 1] = (byte)G;
+                                pixels[pixelOffset + 2] = (byte)R;
+                                pixels[pixelOffset + 3] = (byte)A;
+                                it++;
+                                if (it == Wwt)
+                                {
+                                    ik++;
+                                }
+                            }
+                            else if (ik % 4 == 2)
+                            {
+                                R = Rdat[3 * k];
+                                G = Rdat[3 * k + 1];
+                                B = Rdat[3 * k + 2];
+                                A = 255;
+                                pixelOffset = (dWw + it0 + it + (dHw + jt0 + jt) * wb.PixelWidth) * wb.Format.BitsPerPixel / 8;
+                                pixels[pixelOffset] = (byte)B;
+                                pixels[pixelOffset + 1] = (byte)G;
+                                pixels[pixelOffset + 2] = (byte)R;
+                                pixels[pixelOffset + 3] = (byte)A;
+                                jt--;
+                                if (jt == -1)
+                                {
+                                    ik++;
+                                    //jt0--;
+                                }
+                            }
+                            else
+                            {
+                                R = Rdat[3 * k];
+                                G = Rdat[3 * k + 1];
+                                B = Rdat[3 * k + 2];
+                                A = 255;
+                                pixelOffset = (dWw + it0 + it + (dHw + jt0 + jt) * wb.PixelWidth) * wb.Format.BitsPerPixel / 8;
+                                pixels[pixelOffset] = (byte)B;
+                                pixels[pixelOffset + 1] = (byte)G;
+                                pixels[pixelOffset + 2] = (byte)R;
+                                pixels[pixelOffset + 3] = (byte)A;
+                                it--;
+                                if (it == -1)
+                                {
+                                    it = 0;
+                                    jt = 0;
+                                    ik++;
+                                    it0--;
+                                    jt0--;
+                                    Hwt += 2;
+                                    Wwt += 2;
+                                }
+                            }
+                            int stride = ((int)Image1.Width * wb.Format.BitsPerPixel) / 8;
+                            wb.WritePixels(rect, pixels, stride, 0);                            
+                            k++;
+                        }
+                        // Show the bitmap in an Image element.
+                        Image1.Source = wb;
+                        worker.CancelAsync();
+                    }
+                }
             }
-        }
+            /*for (int i = 0; i < 100; i++)
+            {
+                PBNFT.Value++;
+                await Task.Delay(200);
+            }*/
+        }        
     }
 }
--- SimpleNeurotuner/WelcomeWindow.xaml.cs
+++ SimpleNeurotuner/WelcomeWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace SimpleNeurotuner
                 //File.Create("DataTemp.dat");
                 File.WriteAllText(FileLanguage.FullName, "0");
                 Title = "Добро пожаловать";
-                lbWelcome.Content = "Добро пожаловать в Нейрокейс";
+                lbWelcome.Content = "Добро пожаловать в Нейротюнер NFT";
                 lbDescriptionText.Content = "Версия: 1.1\n\nПрограмма предназначена для курса\nпо входу в ресурсное состояние.\n\nВ данном окне вы можете выбрать язык русский\nили английский.\n\nИнструкция:\n\n1. Если микрофон и динамики (наушники)\nне выбраны по умолчанию, то выберите их самостоятельно.\n\n2. Выберите запись.\n\n3. Нажмите кнопку запуска и наслаждайтесь.\nТакже вы можете за счет ползунка регулировать громкость\nсвоего голоса в микрофоне.\nВозникло желание остановиться, нажми кнопку стоп.\n\n4. Если вы хотите сделать свою запись, то переключите режим\nиз прослушивания в режим записи.\n\n5. У вас появится окошко в котором вы можете\nназвать свою запись.\n\n6. После того как вы назовете свою запись, и нажмите\nкнопку создать вы перейдете в основное окошко, в котором\nпоявится дополнительная кнопка.\n\n7. Чтобы сделать запись начинаете сначала издавать звук,\nа потом нажимаете кнопку старт, тогда начнется запись,\nпосле окончания записи выскочит окошко,\nо завершении записи.\n\n8. Потом вы нажимаете на кнопку прослушать и слушаете\nсвою запись. После того как вы наслушались запись,\nнажимаете кнопку стоп. Выскочит окошко в котором вы\nвыбираете сохранить свою запись, либо удалить\nи перезаписать.";
             } 
             else
@@ -43,7 +43,7 @@ namespace SimpleNeurotuner
                 //File.Create("DataTemp.dat");
                 File.WriteAllText(FileLanguage.FullName, "1");
                 Title = "Welcome";
-                lbWelcome.Content = "Welcome to Neurokeys";
+                lbWelcome.Content = "Welcome to Neurotuner NFT";
                 lbDescriptionText.Content = "Version: 1.1\n\nThe program is intended for a course on entering the resource state.\n\nIn this window, you can select the language Russian or English.\n\nInstruction:\n\n1. If the microphone and speakers (headphones) \nare not selected by default, then select them yourself.\n\n2. Select an entry.\n\n3. Press the start button, and enjoy.You can also use the slider to\nadjust the volume of your voice in the microphone. There was a desire to stop,\npress the stop button.\n\n4. If you want to make your own recording, then switch the\nmode from listening to recording mode.\n\n5. You will have a window in which you can name your entry.\n\n6. After you name your entry and press the create button,\nyou will go to the main window in which an additional\nbutton will appear.\n\n7. To make a recording, first start making a sound, and then\npress the start button, then the recording will begin, after\nthe end of the recording, a window will pop up about\nthe completion of the recording.\n\n8. Then you click on the listen button and listen to your recording.\nAfter you have listened to the recording, press the stop button.\nA window pops up in which you choose to keep your entry,\nor delete and overwrite.";
             }
         }
